3f448bd6d71809482cf7f3faa06c69e58c3c9ab1
Dennismarangu <dennismutumamarangu@gmail.com>
2023-06-07 10:32:04 +0300

    Completed BookingForm.js

--- health_check_app/src/Components/BookingForm.js
+++ health_check_app/src/Components/BookingForm.js
@@ -20,7 +20,39 @@ const BookingForm = () => {
       }
     };
 
-}
+    return (
+        <div>
+          <h2>Booking Form</h2>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Hospital"
+              value={hospital}
+              onChange={(e) => setHospital(e.target.value)}
+            />
+            <input
+              type="date"
+              placeholder="Date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+            <input
+              type="time"
+              placeholder="Time"
+              value={time}
+              onChange={(e) => setTime(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Reason"
+              value={reason}
+              onChange={(e) => setReason(e.target.value)}
+            />
+            <button type="submit">Book Appointment</button>
+          </form>
+        </div>
+      );
+    };
 
 export default BookingForm;
 
