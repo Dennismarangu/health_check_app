8692640744c57c3de48c72397b1fb7df5e366302
Dennismarangu <dennismutumamarangu@gmail.com>
2023-06-07 10:45:00 +0300

    App.js

--- health_check_app/src/App.js
+++ health_check_app/src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
 import './App.css';
+import RegistrationForm from './RegistrationForm';
+import BookingForm from './BookingForm';
+import AppointmentList from './AppointmentList';
 
 function App() {
   return (
     <div className="App">
-  
+      <RegistrationForm />
+      <BookingForm />
+      <AppointmentList />
     </div>
   );
 }
