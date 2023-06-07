1f7ab139556d53508ae0e9f55a96fb218598e7a8
Dennismarangu <dennismutumamarangu@gmail.com>
2023-06-07 10:31:26 +0300

    Updated BookingForm.js

--- health_check_app/src/Components/BookingForm.js
+++ health_check_app/src/Components/BookingForm.js
@@ -2,6 +2,23 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const BookingForm = () => {
+    const [hospital, setHospital] = useState('');
+    const [date, setDate] = useState('');
+    const [time, setTime] = useState('');
+    const [reason, setReason] = useState('');
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      const newAppointment = { hospital, date, time, reason };
+
+      try {
+        // Make a POST request to the backend API to create a new appointment
+        const response = await axios.post('http://localhost:9292/appointments', newAppointment);
+        console.log(response.data); // Handle the response data as needed
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
 }
 
