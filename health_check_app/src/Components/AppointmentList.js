a0d981368365c3a9f5b799f7aa23b539228fe431
Dennismarangu <dennismutumamarangu@gmail.com>
2023-06-07 22:47:49 +0300

    Updated component

--- health_check_app/src/Components/AppointmentList.js
+++ health_check_app/src/Components/AppointmentList.js
@@ -4,6 +4,8 @@ import Navbar from './Navbar';
 
 const AppointmentList = () => {
   const [appointments, setAppointments] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortOrder, setSortOrder] = useState('desc');
 
   useEffect(() => {
     // Fetch the list of appointments from the backend API
@@ -19,12 +21,44 @@ const AppointmentList = () => {
     fetchAppointments();
   }, []);
 
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const handleSortClick = () => {
+    setSortOrder((prevOrder) => (prevOrder === 'desc' ? 'asc' : 'desc'));
+  };
+
+  // Filter and sort appointments based on search term and sort order
+  const filteredAppointments = appointments.filter((appointment) =>
+    appointment.hospital.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const sortedAppointments = filteredAppointments.sort((a, b) => {
+    if (sortOrder === 'asc') {
+      return new Date(a.date) - new Date(b.date);
+    } else {
+      return new Date(b.date) - new Date(a.date);
+    }
+  });
+
   return (
     <div>
-      <Navbar appointmentCount={appointments.length} />
+      <Navbar appointmentCount={sortedAppointments.length} />
+      <div className="appointment-controls">
+        <input
+          type="text"
+          placeholder="Search Hospital"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
+        <button onClick={handleSortClick}>
+          Sort {sortOrder === 'desc' ? 'Newest' : 'Oldest'}
+        </button>
+      </div>
       <h2>Appointments</h2>
       <ul>
-        {appointments.map((appointment) => (
+        {sortedAppointments.map((appointment) => (
           <li key={appointment.id}>
             <p>Hospital: {appointment.hospital}</p>
             <p>Date: {appointment.date}</p>
